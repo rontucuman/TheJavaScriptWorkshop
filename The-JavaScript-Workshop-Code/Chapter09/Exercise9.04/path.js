dca6d7bc1e57221812ea7151852e0d55caadbabf
Ron <rontucuman@gmail.com>
2020-05-16 10:05:14 -0400

    deleting book code

--- The-JavaScript-Workshop-Code/Chapter09/Exercise9.04/path.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-let dir = "/Users/gauravmehla/Documents/node/modules/";
-let otherDir = "/assets/images/";
-let file = "myFile.js";
-
-console.log(path.dirname(dir + file));
-console.log(path.extname(file));
-console.log(path.join(dir, otherDir + file));
